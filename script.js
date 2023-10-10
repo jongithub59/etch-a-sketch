59cd92e84f6a483f2eb56c32cf4fd1d730b5bec9
jongithub59 <jdelrealj01@gmail.com>
2023-10-10 17:03:45 -0500

    added createGrid function that adds divs to page made a for loop that adds 16 square divs with a black border centered on the screen with flexbox and added basic css styles.

---
+++ script.js
@@ -0,0 +1,12 @@
+function createGrid() {
+    for (let i = 0; i < 16; i++) {
+        const square = document.createElement('div');
+        square.classList.add('square') 
+        
+        container.appendChild(square);
+    }
+}
+
+const container = document.getElementById('container');
+
+createGrid();
