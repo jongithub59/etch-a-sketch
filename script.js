7f179882b0f0c0dc4c35bfa508b14c44cb8c3fd4
jongithub59 <jdelrealj01@gmail.com>
2023-10-10 20:52:33 -0500

    made a 16x16 grid of boxes and added new font created a clean square grid ready for etch sketch functionality and different size options to be added later. imported new font.

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 function createGrid() {
-    for (let i = 0; i < 16; i++) {
+    for (let i = 0; i < 256; i++) {
         const square = document.createElement('div');
-        square.classList.add('square') 
-        
+        square.classList.add('square');
+        square.addEventListener('hover', colorSquare);
         container.appendChild(square);
     }
+}
+
+function colorSquare() {
+    
 }
 
 const container = document.getElementById('container');
