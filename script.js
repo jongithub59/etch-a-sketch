cb67577feb8dc8ec5bc3861f7752a2e3a222c491
jongithub59 <jdelrealj01@gmail.com>
2023-10-11 13:21:00 -0500

    added  color change funtionality to grid grid elements now change color to grey when hovered over like an etch-a-sketch by changing its class to one that has a grey color style rule.

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
+
 function createGrid() {
     for (let i = 0; i < 256; i++) {
         const square = document.createElement('div');
         square.classList.add('square');
-        square.addEventListener('hover', colorSquare);
+        square.addEventListener('mouseover', (e) => {
+            // color chage is done by changing it to a class with a grey style 
+            e.target.classList.remove('square');
+            e.target.classList.add('greySquare');
+        });
         container.appendChild(square);
     }
-}
-
-function colorSquare() {
-    
 }
 
 const container = document.getElementById('container');
