dd1f00026204ed5c6c09c4565eb36b997729d35d
jongithub59 <jdelrealj01@gmail.com>
2023-10-13 13:03:40 -0500

    added buttons and size option grid size can now be changed with teh size buttons and drawings can be erased with the erase buttons. fixed some css to make the page more responsive and consistent. button styling will be done later

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
+let size = 16; //default size for grid 16x16 
+const container = document.getElementById('container');
+const eraseButton = document.getElementById('erase-button');
+const normalButton = document.getElementById('normal-button');
+const largeButton = document.getElementById('large-button');
+eraseButton.addEventListener('click', clearGrid);
+normalButton.addEventListener('click', normalSize);
+largeButton.addEventListener('click', largeSize);
 
-function createGrid() {
-    for (let i = 0; i < 256; i++) {
+function createGrid(size) {
+    container.innerHTML = "";
+    for (let i = 0; i < size * size; i++) { // size is multiplied to get the actual amount os squares to fil the grid
         const square = document.createElement('div');
         square.classList.add('square');
+        if (size == 32) {
+            square.style.width = square.style.height = `12.5px`; //makes grid elements smaller if more squares are made
+        }
         square.addEventListener('mouseover', (e) => {
             // color change is done by changing it to a class with a grey style 
             e.target.classList.remove('square');
@@ -11,16 +23,20 @@ function createGrid() {
         container.appendChild(square);
     }
 }
-
-const container = document.getElementById('container');
-var gridSize = document.getElementById('grid-size');
-var slider = document.getElementById('slider');
-gridSize.innerHTML = slider.value; //makes the span with id gridSize display the slider's currnet value
-
-slider.oninput = function() {
-    console.log(this);
-    gridSize.innerHTML = this.value; //updates the span with the current slider value using this
+   
+function clearGrid() {
+    container.innerHTML = "";
+    createGrid(size);
 }
 
+function normalSize() {
+    let size = 16;
+    clearGrid(size);
+}
 
-createGrid();
+function largeSize() {
+    let size = 32;
+    createGrid(size);
+}
+
+createGrid(size);
